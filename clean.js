d1311f14b797ae094736c90426dc5c1682332ca8
QaseemB <Qbarnhardt@yahoo.com>
2024-06-24 23:33:34 -0400

    sba308A

--- clean.js
+++ clean.js
@@ -1,6 +1,8 @@
 import { getLocation } from "./index.js";
+// import {getParkName} from './test.js'
 
 // const parks = await fetchData('https://data.cityofnewyork.us/resource/enfh-gkve.json?$limit=5000');
 
 // let parkSelection = document.querySelector('#park-selection');
 getLocation()
+getParkName()
